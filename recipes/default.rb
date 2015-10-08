32ecbebb12c94f754a9fb3e41aebc5882ae9abbb
Joe Walsh <j.thomas.walsh@gmail.com>
2015-10-07 23:51:15 -0500

    Create default.rb

---
+++ recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: deploybot
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+
+default.anaconda.accept_license = 'yes'
+
+default.anaconda.owner = 'ubuntu'
+default.anaconda.group = 'ubuntu'
+include_recipe 'anaconda::default'
